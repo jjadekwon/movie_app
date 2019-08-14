d16f14c597b08475d7cb7c91ae4be0cbda4c5fd2
jjadekwon <jjade.kwon@gmail.com>
2019-08-14 15:03:17 +0900

    Updates

---
+++ precache-manifest.e90ed5cc42323e51517633b82aba30fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c756e20f329dd8fb22d84aa5b319605",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "3d619d86aecd145bfdc3",
+    "url": "/movie_app/static/css/main.abe87fc1.chunk.css"
+  },
+  {
+    "revision": "c5066daec65e87d52813",
+    "url": "/movie_app/static/js/2.a34f1a90.chunk.js"
+  },
+  {
+    "revision": "3d619d86aecd145bfdc3",
+    "url": "/movie_app/static/js/main.d8d58dc9.chunk.js"
+  },
+  {
+    "revision": "e8487b30ace6bc0fa875",
+    "url": "/movie_app/static/js/runtime~main.310586fb.js"
+  }
+]);
